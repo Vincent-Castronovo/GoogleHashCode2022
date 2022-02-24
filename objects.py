2b384cd1c8ce349f3b05f988a2c97670f9126ef8
LeoRM <leo.renson-miquel@telecomnancy.eu>
2022-02-24 19:17:09 +0100

    update

--- objects.py
+++ objects.py
@@ -16,6 +16,17 @@ class Project:
 
     name = "Project name"
     duration = 0
+    score = 0
     best_duration = 0
+    number_of_roles = 0
     #{roles, level}
     roles = []
+
+    #class default constructor
+    def __init__(self, name, duration, score, best_duration, number_of_roles, roles): 
+        self.name = name
+        self.duration = duration
+        self.score = score
+        self.best_duration = best_duration
+        self.number_of_roles = number_of_roles
+        self.roles = roles
